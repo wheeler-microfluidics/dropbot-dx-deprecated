38547b4d3c74b466f411c43498250067655a1824
Christian Fobel <christian@fobel.net>
2016-04-15 18:23:04 -0400

    [NB] Add method to read temperature/humidity
    Add `get_environment_state` method to read temperature/humidity from
    [Honeywell HIH6000][1] sensor using [i2c][2].
    
    [1]: http://sensing.honeywell.com/honeywell-sensing-humidicon-hih6000-series-product-sheet-009073-7-en2.pdf
    [2]: http://sensing.honeywell.com/index.php/ci_id/142171/la_id/1/document/1/re_id/0

--- dropbot_dx/proxy.py
+++ dropbot_dx/proxy.py
@@ -1,3 +1,5 @@
+import time
+
 from path_helpers import path
 
 
@@ -35,6 +37,41 @@ try:
                 pass
             super(ProxyMixin, self).__del__()
 
+        def get_environment_state(self, i2c_address=0x27):
+            '''
+            Acquire temperature and humidity from Honeywell HIH6000 series
+            sensor.
+
+            [1]: http://sensing.honeywell.com/index.php/ci_id/142171/la_id/1/document/1/re_id/0
+            '''
+            import pandas as pd
+
+            # Trigger measurement.
+            self.i2c_write(i2c_address, [])
+            time.sleep(.01)
+
+            while True:
+                # Read 4 bytes from sensor.
+                humidity_data, temperature_data = self.i2c_read(i2c_address,
+                                                                4).view('>u2')
+                status_code = (humidity_data >> 14) & 0x03
+                if status_code == 0:
+                    # Measurement completed successfully.
+                    break
+                elif status_code > 1:
+                    raise IOError('Error reading from sensor.')
+                # Measurement data is stale (i.e., measurement still in
+                # progress).  Try again.
+                time.sleep(.001)
+
+            # See URL from docstring for source of equations.
+            relative_humidity = float(humidity_data & 0x03FFF) / ((1 << 14) - 2)
+            temperature_celcius = (float((temperature_data >> 2) & 0x3FFF) /
+                                   ((1 << 14) - 2) * 165 - 40)
+
+            return pd.Series([relative_humidity, temperature_celcius],
+                             index=['relative_humidity',
+                                    'temperature_celcius'])
 
     class Proxy(ProxyMixin, _Proxy):
         pass
